ffaf9867234b4c5f7894ea9e3212d3e25b813e8f
Pragyan Adhikari <pragyanadhikari961@gmail.com>
2025-04-28 19:29:55 +0545

    Model Creation

--- MocTest/ioemoctest/firstapp/admin.py
+++ MocTest/ioemoctest/firstapp/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Question, Option, UserAnswer, UserScore
 
 # Register your models here.
+
+admin.site.register(UserAnswer)
+admin.site.register(UserScore)
+
+class OptionInline(admin.TabularInline):
+    model = Option
+    extra = 4  # show 4 empty option fields
+
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [OptionInline]
+
+admin.site.register(Question, QuestionAdmin)
--- MocTest/ioemoctest/firstapp/models.py
+++ MocTest/ioemoctest/firstapp/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Question(models.Model):
+    text = models.TextField()
+
+    def __str__(self):
+        return self.text
+
+class Option(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='options')
+    text = models.CharField(max_length=255)
+    is_correct = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.text
+
+class UserAnswer(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    selected_option = models.ForeignKey(Option, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.question.text[:20]}..."
+
+class UserScore(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    score = models.IntegerField(default=0)
+    date_taken = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.score}"
