096aa9bf2c4b43c623d8aeac990ee7d0c61779df
Pragyan Adhikari <pragyanadhikari961@gmail.com>
2025-05-07 15:38:17 +0545

    views update

--- MocTest/ioemoctest/firstapp/urls.py
+++ MocTest/ioemoctest/firstapp/urls.py
@@ -4,6 +4,10 @@ from firstapp import views
 
 urlpatterns = [
     path('', views.index),
+<<<<<<< HEAD
     path('start_test/<str:subject_id>/', views.start_test, name='start_test'),
     path('submit_test/', views.submit_test, name='submit_test'),
+=======
+    path('question/', views.question_page, name='question_page'),
+>>>>>>> refs/remotes/origin/qnsdisplay
 ]
